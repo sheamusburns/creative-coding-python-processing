0ad212cc0bff3ff4b3adbaa891e1473710afe745
Sheamus Burns <sheamusburns@gmail.com>
2018-02-01 11:47:51 -0500

    update challenge doc to include wall class requirement

--- challenges/wall_class_challenge.py
+++ challenges/wall_class_challenge.py
@@ -1,4 +1,4 @@
-## Write a processing sketch that includes a custom moves a small rectangle to the left of the screen until it hits the edge of the window and then heads in the other direction (right) until it hits the other side. It should continue bouncing back and forth at the same velocity. 
+## Write a processing sketch that includes a custom Wall class, moves a small rectangle to the left of the screen until it hits the edge of the window and then heads in the other direction (right) until it hits the other side. It should continue bouncing back and forth at the same velocity. 
 ## When the user hits the right right arrow key, make the rectangle move to the right. When it hits the left arrow key, make it move to the left. 
 ## Your code should have a setup() function definition, a draw() function definition, and a Wall class definition.
 ## The Wall class should contain attributes and methods that you can call in the draw function (and in other methods) -- I know this sounds confusing but break it down into small pieces.  
