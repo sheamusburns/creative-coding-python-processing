087e4c7150ace8575660740fed3eed0bc835ee5d
Sheamus Burns <sheamusburns@gmail.com>
2018-02-03 20:10:00 -0500

    add wall class challenge

--- challenges/wall_class_challenge.py
+++ challenges/wall_class_challenge.py
@@ -1,5 +1,18 @@
-## Write a processing sketch that includes a custom Wall class, moves a small rectangle to the left of the screen until it hits the edge of the window and then heads in the other direction (right) until it hits the other side. It should continue bouncing back and forth at the same velocity. 
-## When the user hits the right right arrow key, make the rectangle move to the right. When it hits the left arrow key, make it move to the left. 
-## Your code should have a setup() function definition, a draw() function definition, and a Wall class definition.
-## The Wall class should contain attributes and methods that you can call in the draw function (and in other methods) -- I know this sounds confusing but break it down into small pieces.  
-## Extension: translate location and velocity into PVector objects.
+# Write a processing sketch that includes a custom Wall class, 
+# moves a small rectangle to the left of the screen until it 
+# hits the edge of the window and then heads in the other direction 
+# (right) until it hits the other side. It should continue bouncing
+# back and forth at the same velocity. 
+#
+# When the user hits the right right arrow key, 
+# make the rectangle move to the right. When it hits the left 
+# arrow key, make it move to the left. 
+#
+# Your code should at least have a setup() function definition, 
+# a draw() function definition, and a Wall class definition.
+#
+# The Wall class should contain attributes and methods that you 
+# can call in the draw function (and in other methods) -- I know 
+# this sounds confusing but break it down into small pieces.  
+#
+# Extension: translate location and velocity into PVector objects.
--- docs/index.md
+++ docs/index.md
@@ -8,3 +8,10 @@ Each week there will be a number of challenges available that will build on and 
 
 	- [download challenge file](https://github.com/sheamusburns/creative-coding-python-processing/blob/master/challenges/transforming_data__lists_tuples_and_classes.ipynb)
 
+1. Writing A Class with Processing
+
+	- This challenge levels you up in Processing. By using classes, we can have more control over elements on our canvas. Go ahead and try to build a moving wall using a `Wall` class in a new Processing sketch. 
+
+	- Need a boost? Read ch. 10 (pp 129-140) in Make: Getting Started with Processing.py (the yellow book). It's all about using classes to create objects in Processing.
+
+	- [download challenge file](https://github.com/sheamusburns/creative-coding-python-processing/blob/master/challenges/wall_class_challenge.py)
